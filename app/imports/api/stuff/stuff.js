1ca1016792d8ae9eb58775768c9ec6114d9639c5
Jiaying Feng <jiayingf@hawaii.edu>
2018-11-14 22:52:10 -1000

    Commited Deployment

--- app/imports/api/stuff/stuff.js
+++ app/imports/api/stuff/stuff.js
@@ -11,11 +11,14 @@ const StuffSchema = new SimpleSchema({
   courseName: String,
   month: {
     type: String,
-    allowedValues: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October','November', 'December'],
+    allowedValues: ['January', 'February', 'March', 'April',
+      'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
   },
   day: {
     type: Number,
-    allowedValues: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31],
+    allowedValues: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+      11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+      23, 24, 25, 26, 27, 28, 29, 30, 31],
   },
   time: String,
   owner: String,
