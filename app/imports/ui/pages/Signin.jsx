466aed435496b300a525c62d5edc021d9eb77b71
QiQing Luo <qiqingl@hawaii.edu>
2018-12-03 23:27:26 -1000

    commit changes in master

--- app/imports/ui/pages/Signin.jsx
+++ app/imports/ui/pages/Signin.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { Link, Redirect } from 'react-router-dom';
 import { Meteor } from 'meteor/meteor';
 import { Container, Form, Grid, Header, Message, Segment } from 'semantic-ui-react';
+import NavBar from '../components/NavBar';
 
 /**
  * Signin page overrides the form’s submit event and call Meteor’s loginWithPassword().
@@ -47,6 +48,7 @@ export default class Signin extends React.Component {
     // Otherwise return the Login form.
     return (
         <div className='background1'>
+          <NavBar/>
         <Container>
           <Grid textAlign="center" verticalAlign="middle" centered columns={2}>
             <Grid.Column>
