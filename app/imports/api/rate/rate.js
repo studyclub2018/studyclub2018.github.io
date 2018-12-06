70dd0663811818918387e80e8296ea5cd89016ae
Jiaying Feng <jiayingf@hawaii.edu>
2018-12-05 19:15:30 -1000

    commited changes

--- app/imports/api/rate/rate.js
+++ app/imports/api/rate/rate.js
@@ -3,7 +3,7 @@ import SimpleSchema from 'simpl-schema';
 import { Tracker } from 'meteor/tracker';
 
 /** Create a Meteor collection. */
-const Rates = new Mongo.Collection('Stuffs');
+const Rates = new Mongo.Collection('Rates');
 
 /** Create a schema to constrain the structure of documents associated with this collection. */
 const RateSchema = new SimpleSchema({
